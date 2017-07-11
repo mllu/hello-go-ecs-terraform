b5b9f237cb09d74013b0852348966e8aeb55874d
mllu <mornlyn@gmail.com>
2017-07-11 11:11:28 -0700

    initial working version after update deprecated syntax

--- tf/main.tf
+++ tf/main.tf
@@ -4,41 +4,56 @@ variable "appname" {default = "HelloGoEcsTerraform" }
 variable "host_port" { default = 8080 }
 variable "docker_port" { default = 8080 }
 variable "lb_port" { default = 80 }
-variable "aws_region" { default = "us-east-1" }
-variable "key_name" {}
-variable "dockerimg" {}
-
-# From https://github.com/aws/amazon-ecs-cli/blob/d566823dc716a83cf97bf93490f6e5c3c757a98a/ecs-cli/modules/config/ami/ami.go#L31
-variable "ami" {
-  description = "AWS ECS AMI id"
-  default = {
-    us-east-1 = "ami-67a3a90d"
-    us-west-1 = "ami-b7d5a8d7"
-    us-west-2 = "ami-c7a451a7"
-    eu-west-1 = "ami-9c9819ef"
-    eu-central-1 =  "ami-9aeb0af5"
-    ap-northeast-1 = "ami-7e4a5b10"
-    ap-southeast-1 = "ami-be63a9dd"
-    ap-southeast-2 = "ami-b8cbe8db"
-  }
+variable "aws_region" { default = "us-east-2" }
+#variable "key_name" {default = "dev"}
+variable "key_name" {default = "ec2-ohio"}
+variable "dockerimg" {default = "mllu/gohttpserver"}
+variable "az_count" {
+  description = "Number of AZs to cover in a given AWS region"
+  default     = "2"
 }
 
 provider "aws" {
   region = "${var.aws_region}"
 }
 
-module "vpc" {
-  source = "github.com/terraform-community-modules/tf_aws_vpc"
-  name = "${var.appname}-${var.environ}-vpc"
-  cidr = "10.100.0.0/16"
-  public_subnets  = "10.100.101.0/24,10.100.102.0/24"
-  azs = "us-east-1c,us-east-1b"
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "main" {
+    cidr_block = "10.10.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  count             = "${var.az_count}"
+  cidr_block        = "${cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)}"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+  vpc_id            = "${aws_vpc.main.id}"
+}
+
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"
+}
+
+resource "aws_route_table" "r" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  count          = "${var.az_count}"
+  subnet_id      = "${element(aws_subnet.main.*.id, count.index)}"
+  route_table_id = "${aws_route_table.r.id}"
 }
 
 resource "aws_security_group" "allow_all_outbound" {
-  name_prefix = "${var.appname}-${var.environ}-${module.vpc.vpc_id}-"
+  name_prefix = "${var.appname}-${var.environ}-${aws_vpc.main.id}-"
   description = "Allow all outbound traffic"
-  vpc_id = "${module.vpc.vpc_id}"
+  vpc_id = "${aws_vpc.main.id}"
 
   egress = {
     from_port = 0
@@ -49,9 +64,9 @@ resource "aws_security_group" "allow_all_outbound" {
 }
 
 resource "aws_security_group" "allow_all_inbound" {
-  name_prefix = "${var.appname}-${var.environ}-${module.vpc.vpc_id}-"
+  name_prefix = "${var.appname}-${var.environ}-${aws_vpc.main.id}-"
   description = "Allow all inbound traffic"
-  vpc_id = "${module.vpc.vpc_id}"
+  vpc_id = "${aws_vpc.main.id}"
 
   ingress = {
     from_port = 0
@@ -62,9 +77,9 @@ resource "aws_security_group" "allow_all_inbound" {
 }
 
 resource "aws_security_group" "allow_cluster" {
-  name_prefix = "${var.appname}-${var.environ}-${module.vpc.vpc_id}-"
+  name_prefix = "${var.appname}-${var.environ}-${aws_vpc.main.id}-"
   description = "Allow all traffic within cluster"
-  vpc_id = "${module.vpc.vpc_id}"
+  vpc_id = "${aws_vpc.main.id}"
 
   ingress = {
     from_port = 0
@@ -82,9 +97,9 @@ resource "aws_security_group" "allow_cluster" {
 }
 
 resource "aws_security_group" "allow_all_ssh" {
-  name_prefix = "${var.appname}-${var.environ}-${module.vpc.vpc_id}-"
+  name_prefix = "${var.appname}-${var.environ}-${aws_vpc.main.id}-"
   description = "Allow all inbound SSH traffic"
-  vpc_id = "${module.vpc.vpc_id}"
+  vpc_id = "${aws_vpc.main.id}"
 
   ingress = {
     from_port = 22
@@ -126,6 +141,7 @@ resource "aws_iam_policy_attachment" "ecs_for_ec2" {
 # This is the role for the load balancer to have access to ECS.
 resource "aws_iam_role" "ecs_elb" {
   name = "${var.appname}_ecs_elb_${var.environ}"
+  path = "/"
   assume_role_policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -155,7 +171,7 @@ resource "aws_ecs_cluster" "cluster" {
     name = "${var.appname}_${var.environ}"
 }
 
-resource "template_file" "task_definition" {
+data "template_file" "task_definition" {
   depends_on = ["null_resource.docker"]
   template = "${file("task-definition.json.tmpl")}"
   vars {
@@ -170,12 +186,12 @@ resource "template_file" "task_definition" {
 
 resource "aws_ecs_task_definition" "ecs_task" {
   family = "${var.appname}_${var.environ}"
-  container_definitions = "${template_file.task_definition.rendered}"
+  container_definitions = "${data.template_file.task_definition.rendered}"
 }
 
 resource "aws_elb" "service_elb" {
   name = "${var.appname}-${var.environ}"
-  subnets = ["${split(",", module.vpc.public_subnets)}"]
+  subnets  = ["${aws_subnet.main.*.id}"]
   connection_draining = true
   cross_zone_load_balancing = true
   security_groups = [
@@ -216,7 +232,7 @@ resource "aws_ecs_service" "ecs_service" {
   }
 }
 
-resource "template_file" "user_data" {
+data "template_file" "user_data" {
   template = "ec2_user_data.tmpl"
   vars {
     cluster_name = "${var.appname}_${var.environ}"
@@ -225,13 +241,13 @@ resource "template_file" "user_data" {
 
 resource "aws_iam_instance_profile" "ecs" {
   name = "${var.appname}_${var.environ}"
-  roles = ["${aws_iam_role.ecs.name}"]
+  role = "${aws_iam_role.ecs.name}"
 }
 
 resource "aws_launch_configuration" "ecs_cluster" {
   name = "${var.appname}_cluster_conf_${var.environ}"
   instance_type = "t2.micro"
-  image_id = "${lookup(var.ami, var.aws_region)}"
+  image_id = "ami-8b92b4ee"
   iam_instance_profile = "${aws_iam_instance_profile.ecs.id}"
   associate_public_ip_address = true
   security_groups = [
@@ -239,13 +255,13 @@ resource "aws_launch_configuration" "ecs_cluster" {
     "${aws_security_group.allow_all_outbound.id}",
     "${aws_security_group.allow_cluster.id}",
   ]
-  user_data = "${template_file.user_data.rendered}"
+  user_data = "${data.template_file.user_data.rendered}"
   key_name = "${var.key_name}"
 }
 
 resource "aws_autoscaling_group" "ecs_cluster" {
   name = "${var.appname}_${var.environ}"
-  vpc_zone_identifier = ["${split(",", module.vpc.public_subnets)}"]
+  vpc_zone_identifier  = ["${aws_subnet.main.*.id}"]
   min_size = 0
   max_size = 3
   desired_capacity = 3
